de5a2730f8924dfb6c847ed1dd63c055de9d0cda
Sandro Vicente <sandrovicente@gmail.com>
2014-11-15 23:41:47 +0000

    Updated to follow proposed steps

--- getdata-prj/merge_data.R
+++ getdata-prj/merge_data.R
@@ -17,21 +17,10 @@ DATA.ROOT <- "C:\\tmp\\data\\UCI_HAR_Dataset"
 X_test.file <- paste(DATA.ROOT, "test\\X_test.txt", sep="\\")
 X_test <- read.table(X_test.file)
 
-y_test.file <- paste(DATA.ROOT, "test\\y_test.txt", sep="\\")
-y_test <- read.table(y_test.file)
-
 X_train.file <- paste(DATA.ROOT, "train\\X_train.txt", sep="\\")
 X_train <- read.table(X_train.file)
 
-y_train.file <- paste(DATA.ROOT, "train\\y_train.txt", sep="\\")
-y_train <- read.table(y_train.file)
-
-X_data <- rbind(cbind(X_test, y_test), cbind(X_train, y_train))
-
-features.file <- paste(DATA.ROOT, "features.txt", sep="\\")
-features <- read.table(features.file, colClasses="character")
-
-names(X_data) <- c(features[,2], "al") # activity label key 
+X_data <- rbind(X_test, X_train)
 
 # X_data contains data merged from X_test and X_train, with column names from features.txt.
 # 'al' field contains the key for the activity
@@ -42,9 +31,12 @@ names(X_data) <- c(features[,2], "al") # activity label key
 # Assuming that mean() and std() mark the variables that contain mean and std respectively
 #
 
-key.positions =  which(colnames(X_data) == 'al')
-mean.std.positions <- grep("(mean|std)\\(\\)", names(X_data))
-data.mean.std <- X_data[,c(key.positions, mean.std.positions)]
+
+features.file <- paste(DATA.ROOT, "features.txt", sep="\\")
+features <- read.table(features.file, colClasses="character")
+
+mean.std.positions <- grep("(mean|std)\\(\\)", features[,2]) #features contain names at the 2nd column
+X_data.mean.std <- X_data[,mean.std.positions]
 
 #
 # 'data.mean.std' contains mean/std's measurements and identifiers for the activities
@@ -54,13 +46,24 @@ data.mean.std <- X_data[,c(key.positions, mean.std.positions)]
 # 3. Uses descriptive activity names to name the activities in the data set
 #
 
+y_test.file <- paste(DATA.ROOT, "test\\y_test.txt", sep="\\")
+y_test <- read.table(y_test.file)
+
+y_train.file <- paste(DATA.ROOT, "train\\y_train.txt", sep="\\")
+y_train <- read.table(y_train.file)
+
+data.mean.std.act <- cbind(rbind(y_test,y_train), X_data.mean.std)
+
+colnames(data.mean.std.act)[1] = "al"
+
 activity_label.file <- paste(DATA.ROOT, "activity_labels.txt", sep="\\")
 activity_label <- read.table(activity_label.file)
-
 names(activity_label) <- c("al", "activity")
 
-data.mean.std.act <- merge(data.mean.std, activity_label, by="al")
+data.mean.std.act <- merge(data.mean.std.act, activity_label, by="al")
 
 #
 # 4. Appropriately labels the data set with descriptive variable names. 
 #
+
+names(data.mean.std.act) <- c("al", make.names(features[,2][mean.std.positions]), "activity")

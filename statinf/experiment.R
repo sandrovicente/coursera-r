c46b71b87bfdd5ba472fa0a7589b440673d8c0d3
Your Name Here <sandroav@MININT-OVQ524B.europe.corp.microsoft.com>
2015-05-16 20:36:50 +0100

    first commit

---
+++ statinf/experiment.R
@@ -0,0 +1,25 @@
+## Generate samples 
+nosim <- 1000 # number simulations
+n <- 40
+lambda <- 0.2 
+
+m.dist <- 1/lambda
+sd.dist <- 1/lambda
+
+ALL <- rexp(nosim*n, lambda)
+MEAN <- matrix(ALL, nosim)
+
+means <- apply(MEAN, 1, mean)
+
+m.all <- mean(ALL)
+m.means <- mean(means)
+
+sd.all <- sd(ALL)
+sd.n <- sd.all/sqrt(n)
+sd.means <- sd(means)
+
+library(ggplot2)
+
+M <- data.frame(x=means)
+
+ggplot(M, aes(x=x)) + geom_histogram(alpha=0.3,binwidth=.1, aes(y= ..density..)) + stat_function(fun=dnorm, args=list(mean=m.means, sd=sd.means)) 

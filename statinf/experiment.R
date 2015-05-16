b8c109fda34f47211dd987fbb3378f1a07f8ac0e
Sandro Vicente <sandrovicente@gmail.com>
2015-05-17 00:57:35 +0100

    first experiments part 2

--- statinf/experiment.R
+++ statinf/experiment.R
@@ -25,3 +25,65 @@ ggplot(DF, aes(x=x)) + geom_histogram(alpha=0.3,binwidth=.1, aes(y= ..density..)
 
 DF.ALL <- data.frame(values=ALL)
 ggplot(DF.ALL, aes(x=values)) + geom_histogram(binwidth=.1, aes(y= ..density..)) + xlim(0, 35) + geom_vline(aes(xintercept=5), colour="red")
+
+##############
+
+library(datasets)
+
+summary(ToothGrowth)
+
+tg <- ToothGrowth
+tg$dose <- factor(tg$dose)
+
+## shows clusters
+ggplot(data=tg, aes(x=len, y=dose, group=supp, colour=supp)) +
+    geom_point() 
+
+# http://cran.rstudio.com/web/packages/dplyr/vignettes/introduction.html
+# http://www.cookbook-r.com/Graphs/index.html
+
+library(dplyr)
+
+d2 <- tg %>% filter(dose=="2") %>% select(len)
+d1 <- tg %>% filter(dose=="1") %>% select(len)
+d1_2 <- tg %>% filter(dose=="0.5") %>% select(len)
+
+calc_interval <- function(x, conf) {
+    quantile <- 1-(1-conf)/2
+    (mean(x)+c(-1,1)*qnorm(quantile)*sd(x)/sqrt(length(x)))    
+} 
+
+confidence <- 0.99
+
+d2 <- tg %>% filter(dose=="2") %>% select(len)
+d1 <- tg %>% filter(dose=="1") %>% select(len)
+d1_2 <- tg %>% filter(dose=="0.5") %>% select(len)
+
+calc_interval(d2$len, confidence)
+calc_interval(d1$len, confidence)
+calc_interval(d1_2$len, confidence)
+
+doj <- tg %>% filter(supp=="OJ") %>% select(len)
+dvc <- tg %>% filter(supp=="VC") %>% select(len)
+
+calc_interval(doj$len, confidence)
+calc_interval(dvc$len, confidence)
+
+confidence <- 0.95
+
+d2.oj <- tg %>% filter(dose=="2", supp=="OJ") %>% select(len)
+d1.oj <- tg %>% filter(dose=="1" ,supp=="OJ") %>% select(len)
+d1_2.oj <- tg %>% filter(dose=="0.5", supp=="OJ") %>% select(len)
+
+calc_interval(d2.oj$len, confidence)
+calc_interval(d1.oj$len, confidence)
+calc_interval(d1_2.oj$len, confidence)
+
+
+d2.vc <- tg %>% filter(dose=="2", supp=="VC") %>% select(len)
+d1.vc <- tg %>% filter(dose=="1" ,supp=="VC") %>% select(len)
+d1_2.vc <- tg %>% filter(dose=="0.5", supp=="VC") %>% select(len)
+
+calc_interval(d2.vc$len, confidence)
+calc_interval(d1.vc$len, confidence)
+calc_interval(d1_2.vc$len, confidence)

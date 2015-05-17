5e0dbb933fea0da75e0ee02de7b909595a15c51e
Sandro Vicente <sandrovicente@gmail.com>
2015-05-17 21:22:41 +0100

    included some useful plotting stuff

--- statinf/experiment.R
+++ statinf/experiment.R
@@ -58,47 +58,102 @@ for (supp in levels(tg$supp)) {
     }
 }
 
+
+for (supp in levels(tg$supp)) {
+        print(dose)
+        print(supp)
+        dose.supp <- tg[tg$supp==supp,"len"]
+        print(calc_interval(dose.supp, confidence))
+}
+
 # http://cran.rstudio.com/web/packages/dplyr/vignettes/introduction.html
 # http://www.cookbook-r.com/Graphs/index.html
 
 library(dplyr)
 
+g <- ggplot(tg, aes(x=len)) 
+
 d2 <- tg %>% filter(dose=="2") %>% select(len)
 d1 <- tg %>% filter(dose=="1") %>% select(len)
 d1_2 <- tg %>% filter(dose=="0.5") %>% select(len)
 
-calc_interval2 <- function(x, conf) {
+#####
+library(datasets)
+library(ggplot2)
+library(plyr)
+
+calc_interval <- function(x, conf) {
     quantile <- 1-(1-conf)/2
-    sd <- sd(x)/sqrt(length(x))
+    n <- length(x)
+    sd <- sd(x)
     m <- mean(x)
-    interval <- (mean(x)+c(-1,1)*qnorm(quantile)*sd(x)/sqrt(length(x)))    
-    list(quantile=quantile, sd=sd, m=m, interval=interval)
+    interval <- (m+c(-1,1)*qnorm(quantile)*sd)    
+    list(quantile=quantile, n=n, sd=sd, m=m, interval=interval)
 } 
 
-colours <- c("green", "blue", "magenta", "red", "orange", "black")
-g <- ggplot(tg, aes(x=len))
-i <- 1
-for (supp in levels(tg$supp)) {
-    x <- tg[tg$supp==supp,"len"]
-    l <- calc_interval2(x, confidence)
-    print(supp); print(i)
-    g <- g + stat_function(fun=dnorm, args=list(mean=l$m, sd=l$sd), colour=colours[i], aes(colour = tg$supp ))
-    i <- i + 1
+tg <- ToothGrowth
+tg$dose <- factor(tg$dose)
+tg$supp <- factor(tg$supp)
+
+len.min <- min(tg$len)
+len.max <- max(tg$len)
+len.x <- seq(len.min, len.max, length=100)
+
+confidence <- 0.95
+
+## data for dosage
+
+data.type <- data.frame()
+norm.type <- data.frame()
+for (dose in levels(tg$dose)) {
+    x <- tg[tg$dose==dose,"len"]
+    ds <- data.frame(x=x, type=dose)
+    data.type <- rbind(data.type, ds)
+    
+    l <- calc_interval(x, confidence)
+    norm.type <- rbind(norm.type, data.frame(m=l$m, sd=l$sd, type=dose))
 }
-g + scale_colour_manual("Lgend title", values = colours)
+
+calc_normal <- function(nt) {
+    ddply(norm.type, "type", function(df) {
+        data.frame( 
+            predicted = len.x,
+            density = dnorm(len.x, mean=df$m, sd=df$sd)
+        )
+    })
+}
+
+normaldens <- calc_normal(norm.type)
+
+ggplot(data.type, aes(x=x, fill=type)) +  geom_histogram(alpha = 0.5, aes(y = ..density..), position = 'identity') +
+    geom_line(data=normaldens, aes(x = predicted, y=density)) +
+    facet_wrap(~ type, ncol=3)
+
+ggplot(data.type, aes(x=x, fill=type)) +  geom_histogram(alpha = 0.5, aes(y = ..density..), position = 'identity') +
+    geom_line(data=normaldens, aes(x=predicted, y=density, color=type))
 
 
+## data for all combinations
+
+data.type <- data.frame()
+norm.type <- data.frame()
 for (supp in levels(tg$supp)) {
     for (dose in levels(tg$dose)) {
         x <- tg[tg$supp==supp & tg$dose==dose,"len"]
-        l <- calc_interval2(x, confidence)
-        print(colours[i])
-        g <- g + stat_function(fun=dnorm, args=list(mean=l$m, sd=l$sd), colour=colours[i])
-        i <- i + 1
+        l <- calc_interval(x, confidence)
+        
+        ds <- data.frame(x=x, type=paste(supp,dose,sep="-"))
+        data.type <- rbind(data.type, ds)
+        norm.type <- rbind(norm.type, data.frame(m=l$m, sd=l$sd, type=paste(supp,dose,sep="-")))
     }
 }
-g
 
-g + scale_fill_identity(name = 'the fill', guide = 'legend',labels = c('m1')) +
-    scale_colour_manual(name = 'the colour', 
-                        values =c('black'='black','red'='red'), labels = c('c2','c1'))
+normaldens <- calc_normal(norm.type)
+
+ggplot(data.type, aes(x=x, fill=type)) +  geom_histogram(alpha = 0.5, aes(y = ..density..), position = 'identity') +
+    geom_line(data=normaldens, aes(x = predicted, y=density)) +
+    facet_wrap(~ type, ncol=3)
+
+ggplot(data.type, aes(x=x, fill=type)) +  geom_histogram(alpha = 0.5, aes(y = ..density..), position = 'identity') +
+    geom_line(data=normaldens, aes(x=predicted, y=density, color=type))
+

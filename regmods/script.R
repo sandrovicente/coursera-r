c0b782f6ba4a14a5c9117912100f1710c45b3495
Sandro Vicente <sandrovicente@gmail.com>
2015-06-18 01:35:34 +0100

    more code for report

--- regmods/script.R
+++ regmods/script.R
@@ -8,15 +8,38 @@ data(mtcars)
 
 # am - transmission: 0= automatic, 1= manual
 
+# Did the student interpret the coefficients correctly?
+
+# Did the student do some exploratory data analyses?
+
+# Did the student fit multiple models and detail their strategy for model selection?
+
+# Did the student answer the questions of interest or detail why the question(s) is (are) not answerable?
+
+# Did the student do a residual plot and some diagnostics?
+
+# Did the student quantify the uncertainty in their conclusions and/or perform an inference correctly?
+
+# Was the report brief (about 2 pages long) for the main body of the report and no longer than 5 with supporting appendix of figures?
+
+
 # charts of few measurements (non factors) against mpg
-par(mfrow=c(2,3))
+par(mfrow=c(3,3))
 with(mtcars, plot(disp, mpg, pch=19, col=am+1))
 with(mtcars, plot(hp, mpg, pch=19, col=am+1)) # good
 with(mtcars, plot(drat, mpg, pch=19, col=am+1))
 with(mtcars, plot(wt, mpg, pch=19, col=am+1))
 with(mtcars, plot(qsec, mpg, pch=19, col=am+1)) # good
+
+with(mtcars, plot(gear, mpg, pch=19, col=am+1))
+with(mtcars, plot(carb, mpg, pch=19, col=am+1))
+with(mtcars, plot(cyl, mpg, pch=19, col=am+1))
+
 with(mtcars, plot(am, mpg, pch=19, col=am+1))
+
 legend("topright", pch = 19, col = c(0,1)+1, legend = c("Auto", "Man"))
+
+
 
 # didn't get this chart yet...
 par(mfrow=c(2,2)) 
@@ -38,6 +61,10 @@ fit_qsec_hp <- lm(mpg ~ qsec + hp + am, data=mtcars )
 
 anova(fit_qsec, fit2_qsec, fit_qsec_hp)
 
+fit_qsec_hp2 <- lm(mpg ~ qsec * hp + am, data=mtcars)
+
+anova(fit_qsec, fit2_qsec, fit_qsec_hp, fit_qsec_hp2)
+
 mpg0 <- mtcars[mtcars$am==0, "mpg"]
 mpg1 <- mtcars[mtcars$am==1, "mpg"]
 
@@ -47,3 +74,19 @@ sd(mpg0)/sqrt(length(mpg0))
 mean(mpg1)
 sd(mpg1)/sqrt(length(mpg1))
 
+t.test(mpg0, mpg1, paired=F)
+
+fit_wt <- lm(mpg ~ wt, data=mtcars)
+fit2_wt <- lm(mpg ~ wt + factor(am), data=mtcars)
+anova(fit_wt, fit2_wt)$Pr[2]
+
+###--
+
+vars <- c("cyl", "disp", "hp", "drat", "wt", "qsec", "vs", "am", "gear", "carb" )
+for (v in vars) {
+    a<-anova(lm(mpg ~ wt, data=mtcars), lm(as.formula(paste("mpg ~ wt ", v, sep="+")), data=mtcars))
+    print(v); print(a$Pr[2])
+    if (a$Pr[2] < 0.05) {
+        print(a)
+    }
+}
